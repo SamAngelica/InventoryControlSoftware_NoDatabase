f92f59f20949d83e4e8a1f90d92a482eb0edbb73
Samara Angélica <contato.angelica07@gmail.com>
2025-03-02 19:57:58 -0300

    Update Produtos.cs

--- Projeto PI - Software de estoque/Produtos.cs
+++ Projeto PI - Software de estoque/Produtos.cs
@@ -1,8 +1,8 @@
-class Produto{
+class ControleProduto{
     public string Licença {get; set;}
     public string NomeItem {get; set;}
     public int SKU {get; set;}
     public double Preço {get; set;}
     public string Fornecedor {get; set;}
-    public int Estoque {get; set;}
+    public int EstoqueProduto {get; set;}
 }
